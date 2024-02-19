d5024e375093621bba744c0793adcc9fb764ceab
Tirth Patel <108384786+Tirth4134@users.noreply.github.com>
2024-02-19 16:47:17 +0530

    #

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,70 +1,37 @@
-// import {NextAuthOptions} from "next-auth";
-// import {PrismaAdapter} from "@auth/prisma-adapter"
-// import { Adapter } from "next-auth/adapters";
-// import GoogleProvider from "next-auth/providers/google"
-// import NextAuth from "next-auth";
-// import { env } from "@/lib/env";
-// import { mergeAnonymousCartIntoUserCart } from "@/lib/db/cart";
-// import prisma from "@/lib/db/prisma";
-
-// export const authOptions: NextAuthOptions = {
-//   adapter: PrismaAdapter(prisma) as Adapter,
-//   providers: [
-//     GoogleProvider({
-//         clientId: env.GOOGLE_CLIENT_ID,
-//         clientSecret: env.GOOGLE_CLIENT_SECRET,
-//     })
-//   ],
-//   callbacks:{
-//     session({session, user}) {
-//          session.user.id = user.id
-//          return session;
-//     },
-//   },
-//   events:{
-//      async signIn({user}){
-//         await mergeAnonymousCartIntoUserCart(user.id);
-//     },
-//   },
-// };
-
-
-// const handler = NextAuth(authOptions);
-
-
-// export { handler as GET, handler as POST }
-
-
-import { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import {NextAuthOptions} from "next-auth";
+import {PrismaAdapter} from "@auth/prisma-adapter"
 import { Adapter } from "next-auth/adapters";
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google"
 import NextAuth from "next-auth";
 import { env } from "@/lib/env";
 import { mergeAnonymousCartIntoUserCart } from "@/lib/db/cart";
 import prisma from "@/lib/db/prisma";
 
-const adapter = PrismaAdapter(prisma) as Adapter;
-
-const options: NextAuthOptions = {
-  adapter,
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-    }),
+        clientId: env.GOOGLE_CLIENT_ID,
+        clientSecret: env.GOOGLE_CLIENT_SECRET,
+    })
   ],
-  callbacks: {
-    session({ session, user }) {
-      session.user.id = user.id;
-      return session;
+  callbacks:{
+    session({session, user}) {
+         session.user.id = user.id
+         return session;
     },
   },
-  events: {
-    async signIn({ user }) {
-      await mergeAnonymousCartIntoUserCart(user.id);
+  events:{
+     async signIn({user}){
+        await mergeAnonymousCartIntoUserCart(user.id);
     },
   },
 };
 
-export default NextAuth(options);
+
+const handler = NextAuth(authOptions);
+
+
+export { handler as GET, handler as POST }
+
+
